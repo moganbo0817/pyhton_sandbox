06d3ddf2d55c8a6e9c0faccdf909fd658e83dc29
moganbo0817 <moganbo.moganbo@gmail.com>
2023-02-14 21:41:57 +0900

    fea

--- python/src/gosa.py
+++ python/src/gosa.py
@@ -1,3 +1,5 @@
+from sklearn.metrics import classification_report
+from sklearn import tree
 from sklearn.metrics import mean_absolute_error
 import math
 from sklearn.metrics import mean_squared_error
@@ -23,18 +25,32 @@ mse = mean_squared_error(pred, t)
 # print(math.sqrt(mse))
 
 
-yosoku = [2, 3, 5, 7, 11, 13]
-target = [3, 5, 8, 11, 16, 19]
+# yosoku = [2, 3, 5, 7, 11, 13]
+# target = [3, 5, 8, 11, 16, 19]
 
-mse = mean_squared_error(yosoku, target)
-print('rmse:{}'.format(math.sqrt(mse)))
-print('mae:{}'.format(mean_absolute_error(yosoku, target)))
+# mse = mean_squared_error(yosoku, target)
+# print('rmse:{}'.format(math.sqrt(mse)))
+# print('mae:{}'.format(mean_absolute_error(yosoku, target)))
 
-print('はずれ値の代入')
+# print('はずれ値の代入')
 
-yosoku = [2, 3, 5, 7, 11, 46]
-target = [3, 5, 8, 11, 16, 23]
+# yosoku = [2, 3, 5, 7, 11, 46]
+# target = [3, 5, 8, 11, 16, 23]
 
-mse = mean_squared_error(yosoku, target)
-print('rmse:{}'.format(math.sqrt(mse)))
-print('mae:{}'.format(mean_absolute_error(yosoku, target)))
+# mse = mean_squared_error(yosoku, target)
+# print('rmse:{}'.format(math.sqrt(mse)))
+# print('mae:{}'.format(mean_absolute_error(yosoku, target)))
+
+df = pd.read_csv(parent.joinpath('data/Survived.csv'))
+df = df.fillna(df.mean())
+
+x = df[['Pclass', 'Age']]
+t = df['Survived']
+
+model = tree.DecisionTreeClassifier(max_depth=2, random_state=0)
+model.fit(x, t)
+
+
+pred = model.predict(x)
+out_put = classification_report(y_pred=pred, y_true=t, output_dict=True)
+print(pd.DataFrame(out_put))
